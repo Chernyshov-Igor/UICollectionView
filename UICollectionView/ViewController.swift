7a65d4923f7a4e7336b6e04d14cc2de68ac5fef7
Chernyshov Igor <chernyshov.ig@yandex.ru>
2022-06-23 14:34:39 +0300

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Игорь Чернышов on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
